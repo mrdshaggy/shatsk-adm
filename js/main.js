19a4b60f1820857798eb2bf62bd30c4d678610fa
mrdshaggy <mrdshaggy@gmail.com>
2020-03-17 01:07:01 +0200

    dash

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('main.js');
+
+// Period scroll
+var content = document.getElementById('pRange'),
+    scrollStep = 100;
+
+document.getElementById('pRangeRight').addEventListener('click', function(e) {
+  e.preventDefault();
+  let sl = content.scrollLeft,
+      cw = content.scrollWidth;
+	
+  if ((sl + scrollStep) >= cw) {
+    content.scrollTo(cw, 0);
+  } else {
+    content.scrollTo((sl + scrollStep), 0);
+  }
+});
+
+document.getElementById('pRangeLeft').addEventListener('click', function(e) {
+  e.preventDefault();
+  let sl = content.scrollLeft;
+	
+  if ((sl - scrollStep) <= 0) {
+    content.scrollTo(0, 0);
+  } else {
+    content.scrollTo((sl - scrollStep), 0);
+  }
+});
