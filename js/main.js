481e97a895673be961f64db6ad225298db8fbb44
mrdshaggy <mrdshaggy@gmail.com>
2020-03-23 23:31:48 +0200

    calendar changes / booking page

--- js/main.js
+++ js/main.js
@@ -35,15 +35,58 @@ document.getElementById('pRangeLeft').addEventListener('click', function(e) {
 //
 // Guests today
 //
-var days = document.querySelectorAll('.calendar__day');
+var guestBtn = document.querySelector('.guests-btn');
+var guestCloseBtn = document.querySelector('.guests-close-btn');
 var todayGuests = document.querySelector('.guests');
-for (var i=0;i<days.length;i++) {
-  days[i].addEventListener('click', toggleGuests);
-}
+guestBtn.addEventListener('click', toggleGuests);
+guestCloseBtn.addEventListener('click', toggleGuests);
 function toggleGuests() {
   if (todayGuests.classList.contains('is-active')) {
     todayGuests.classList.remove('is-active');
   } else {
     todayGuests.classList.add('is-active');
   }
+}
+
+
+//
+// Calendar actions
+//
+var days = document.querySelectorAll('.calendar__day');
+var clicks = 0;
+
+
+// for (var i=0;i<days.length;i++) {
+//   days[i].addEventListener('click', makeActive());
+// }
+days.forEach(function(el){
+  // console.log(el);
+  el.addEventListener('click', makeActive);
+});
+function makeActive() {
+  clicks++;
+  console.log(clicks);
+
+  if (clicks == 1) {
+    this.setAttribute("data-selected", "selected");
+    this.setAttribute("data-position", "single");
+    console.log('1');
+  }
+  else if (clicks >= 2) {
+    var single = document.querySelector('[data-position="single"]');
+    single.setAttribute("data-position", "first");
+    
+    this.setAttribute("data-selected", "selected");
+    this.setAttribute("data-position", "last");
+
+    var first = document.querySelector('[data-position="first"]');
+    var last = document.querySelector('[data-position="last"]');
+
+    console.log(first);
+    var nextFirst = first.nextElementSibling;
+    console.log(nextFirst);
+
+    console.log('2');
+  }
+
 }
